675e0b992ed1a0d148339fcf86abf04417f40844
matthewjholmes <>
2021-03-30 19:01:47 -0600

    Render method and Matt's Rabbit Hole updates

--- lib/matt_rabbit_hole.rb
+++ lib/matt_rabbit_hole.rb
@@ -1,18 +1,19 @@
-# @target = nil
-#
-# if ship != nil && target != nil && ship.sunk? == true, then condition = sunk_ship
-# elsif ship != nil && target != nil && ship.sunk? == false && ship.hit == true, then condition = hit_ship
-# elsif ship != nil && target == nil, then condition = unhit_ship
-# elsif ship != nil && target != nil, then condition = unhit_ship
-# elsif ship == nil && target != nil, then condition = missed_shot
-# else ship == nil && target == nil, then condition = open_water
-#
-# def condition class
-#   {
-#     open_water => ".",
-#     unhit_ship => "S",
-#     hit_ship => "H",
-#     missed_shot => "M"
-#     sunk_ship => "X"
-#     }
-# end
+
+#for a method in the Cell class
+
+def render
+  cell_condition = {
+      :open_water => ".",
+      :unhit_ship => "S",
+      :hit_ship => "H",
+      :missed_shot => "M",
+      :sunk_ship => "X"
+      }
+if ship != nil && target != nil && ship.sunk? == true, then condition = sunk_ship
+elsif ship != nil && target != nil && ship.sunk? == false && ship.hit == true, then condition = hit_ship
+elsif ship != nil && target == nil, then condition = unhit_ship
+elsif ship != nil && target != nil, then condition = unhit_ship
+elsif ship == nil && target != nil, then condition = missed_shot
+else ship == nil && target == nil, then condition = open_water
+
+end
