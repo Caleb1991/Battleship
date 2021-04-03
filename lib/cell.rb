f21a0685e44f579035470672fbcb9c805894be6d
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-03 17:19:06 -0600

    Add comment to rethink render conditional

--- lib/cell.rb
+++ lib/cell.rb
@@ -31,13 +31,14 @@ class Cell
   def render(ships = false)
     if fired_upon? == false && @occupied == false
       "."
-    elsif ships == true && @occupied == true
+    elsif ships == true && @occupied == true && fired_upon? != true
       "S"
     elsif fired_upon? == true && @occupied == false
       "M"
     elsif ship.health == 0
       "X"
     elsif fired_upon? == true && @occupied == true
+      # look at the possibilities for if a ship is there and if one isn't 
       "H"
     end
   end
