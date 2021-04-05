256a46fd8be466f181dd4adc34c4d693f4df3d86
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-05 12:00:30 -0600

    Update requirements for class

--- lib/game.rb
+++ lib/game.rb
@@ -2,8 +2,10 @@ class Game
 
 #this should include things such as a main menu
 # ship placement
+# firing
 # turns
-
+# define game over that takes in an argument or maybe make this an attribute and call it in a game
+#over method and then that will break my loop
 
 
 end
