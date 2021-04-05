526029a3d8caf1bf2a0e41d26df4dd24aff74798
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-05 11:39:28 -0600

    Create runner file

---
+++ lib/battleship_runner.rb
@@ -0,0 +1,28 @@
+require './lib/board'
+require './lib/cell'
+require './lib/computer'
+require './lib/game'
+require './lib/player'
+require './lib/ship'
+
+board = Board.new
+
+puts board.render
+
+crusier = Ship.new("Crusier", 3)
+
+board.place(crusier, ["C2", "C3", "C4"])
+
+puts board.render(true)
+
+puts board.render
+
+board.cells["A1"].fire_upon
+
+puts board.render
+
+board.cells["C2"].fire_upon
+board.cells["C3"].fire_upon
+board.cells["C4"].fire_upon
+
+puts board.render
