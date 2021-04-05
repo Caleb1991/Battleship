717e64ccb59d696843105be9a8cd7bb3631ea7c3
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-05 15:09:08 -0600

    Create tests to test class and fire method

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -16,4 +16,33 @@ RSpec.describe Computer do
 
     expect(computer.name).to eq('Roald')
   end
+
+  it 'has a cruiser' do
+    computer = Computer.new
+
+    expect(computer.computer_cruiser).to be_an_instance_of(Ship)
+  end
+
+  it 'has a sub' do
+    computer = Computer.new
+
+    expect(computer.computer_sub).to be_an_instance_of(Ship)
+  end
+
+  it 'has fire coordinates' do
+    computer = Computer.new
+
+    expected = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+
+    expect(computer.fire_coordinates).to eq(expected)
+  end
+
+  it 'fires'do
+    computer = Computer.new
+    current_coordiantes = computer.fire_coordinates.length
+
+    computer.fire
+
+    expect(current_coordiantes).to eq(computer.fire_coordinates.length + 1)
+  end
 end
