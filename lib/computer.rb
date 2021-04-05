2f2c10152bc41367625ad95397a6b982fdc856b0
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-05 08:01:04 -0600

    Gave computer a name and set outline for placing ships

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,8 +1,17 @@
 
 class Computer
+  attr_reader :name
 
-  #have own board
-  #randomly place ships
-  # randomly fire
-  #not fire on previously fired upon coordinates
+  def initialize
+    @name = 'Roald'
+  end
+
+  def place_ships
+    
+  end
 end
+
+#have own board (will happen on runner file)
+#randomly place ships
+# randomly fire
+#not fire on previously fired upon coordinates
