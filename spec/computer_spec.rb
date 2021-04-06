25653f5e24b1fe0d116a30b72d7a7f2c5ba3b1e0
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-06 08:55:47 -0600

    Create tests for available coordinates

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -37,7 +37,101 @@ RSpec.describe Computer do
     expect(computer.fire_coordinates).to eq(expected)
   end
 
-  it 'fires'do
+  it 'creates horizontal cruiser coordiantes' do
+    computer = Computer.new
+
+    computer.create_horizontal_cruiser_coordinates
+
+    expected = [["A1", "A2", "A3"],
+                ["A2", "A3", "A4"],
+                ["A3", "A4", "B1"],
+                ["A4", "B1", "B2"],
+                ["B1", "B2", "B3"],
+                ["B2", "B3", "B4"],
+                ["B3", "B4", "C1"],
+                ["B4", "C1", "C2"],
+                ["C1", "C2", "C3"],
+                ["C2", "C3", "C4"],
+                ["C3", "C4", "D1"],
+                ["C4", "D1", "D2"],
+                ["D1", "D2", "D3"],
+                ["D2", "D3", "D4"]]
+
+    expect(computer.cruiser_ship_coordinates).to eq(expected)
+  end
+
+  it 'creates vertical cruiser coordiantes' do
+    computer = Computer.new
+
+    computer.create_vertical_cruiser_coordinates
+
+    expected = [["A1", "B1", "C1"],
+                ["B1", "C1", "D1"],
+                ["C1", "D1", "B2"],
+                ["D1", "B2", "C2"],
+                ["B2", "C2", "D2"],
+                ["C2", "D2", "A2"],
+                ["D2", "A2", "C3"],
+                ["A2", "C3", "B3"],
+                ["C3", "B3", "D3"],
+                ["B3", "D3", "A3"],
+                ["D3", "A3", "D4"],
+                ["A3", "D4", "B4"],
+                ["D4", "B4", "A4"],
+                ["B4", "A4", "C4"]]
+
+    expect(computer.cruiser_ship_coordinates).to eq(expected)
+  end
+
+  it 'creates horizontal sub coordinates' do
+    computer = Computer.new
+
+    computer.create_horizontal_sub_coordinates
+
+    expected = [["A1", "A2"],
+                ["A2", "A3"],
+                ["A3", "A4"],
+                ["A4", "B1"],
+                ["B1", "B2"],
+                ["B2", "B3"],
+                ["B3", "B4"],
+                ["B4", "C1"],
+                ["C1", "C2"],
+                ["C2", "C3"],
+                ["C3", "C4"],
+                ["C4", "D1"],
+                ["D1", "D2"],
+                ["D2", "D3"],
+                ["D3", "D4"]]
+
+    expect(computer.sub_ship_coordinates).to eq(expected)
+  end
+
+  it 'creates vertical sub coordinates' do
+    computer = Computer.new
+
+    computer.create_vertical_sub_coordinates
+
+    expected = [["A1", "B1"],
+                ["B1", "C1"],
+                ["C1", "D1"],
+                ["D1", "B2"],
+                ["B2", "C2"],
+                ["C2", "D2"],
+                ["D2", "A2"],
+                ["A2", "C3"],
+                ["C3", "B3"],
+                ["B3", "D3"],
+                ["D3", "A3"],
+                ["A3", "D4"],
+                ["D4", "B4"],
+                ["B4", "A4"],
+                ["A4", "C4"]]
+
+    expect(computer.sub_ship_coordinates).to eq(expected)
+  end
+
+  it 'fires' do
     computer = Computer.new
     current_coordiantes = computer.fire_coordinates.length
 
