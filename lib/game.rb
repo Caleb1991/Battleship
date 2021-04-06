8c01c643588945e707105659886e1aec4ee87d40
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-06 17:51:26 -0600

    Remove blank space and refactor spacing

--- lib/game.rb
+++ lib/game.rb
@@ -6,62 +6,62 @@ require './lib/computer'
 
 class Game
 
-def initialize
-  @player_one = Player.new
-  @computer = Computer.new
-end
-
-def computer_fire
-  choice = @computer.fire_coordinates.sample
-
-  @player_one.player_board.cells[choice].fire_upon
-  @computer.fire_coordinates.delete(choice)
-end
-
-def player_fire(coordinate)
-  if @computer.computer_board.valid_coordinate(coordinate) && @computer.computer_board.cells[coordinate].fired_upon? == false
-    @computer.computer_board.cells[coordinate].fire_upon
-    true
-  else
-    puts 'Sorry, that coordinate does not exist, or has already been fired upon, please enter another'
-    false
+  def initialize
+    @player_one = Player.new
+    @computer = Computer.new
   end
-end
+
+  def computer_fire
+    choice = @computer.fire_coordinates.sample
+
+    @player_one.player_board.cells[choice].fire_upon
+    @computer.fire_coordinates.delete(choice)
+  end
+
+  def player_fire(coordinate)
+    if @computer.computer_board.valid_coordinate(coordinate) && @computer.computer_board.cells[coordinate].fired_upon? == false
+      @computer.computer_board.cells[coordinate].fire_upon
+      true
+    else
+      puts 'Sorry, that coordinate does not exist, or has already been fired upon, please enter another'
+      false
+    end
+  end
 
 
-def new_game
-  @player_one = Player.new
-  @computer = Computer.new
-end
+  def new_game
+    @player_one = Player.new
+    @computer = Computer.new
+  end
 
-def computer_places_ships
-  @computer.create_horizontal_cruiser_coordinates
-  @computer.create_vertical_cruiser_coordinates
-  @computer.create_horizontal_sub_coordinates
-  @computer.create_vertical_sub_coordinates
-  @computer.place_cruiser
-  @computer.place_sub
-end
+  def computer_places_ships
+    @computer.create_horizontal_cruiser_coordinates
+    @computer.create_vertical_cruiser_coordinates
+    @computer.create_horizontal_sub_coordinates
+    @computer.create_vertical_sub_coordinates
+    @computer.place_cruiser
+    @computer.place_sub
+  end
 
-def main_menu
- puts "Welcome to....!@!@ Battleship @!@! \n
+  def main_menu
+    puts "Welcome to....!@!@ Battleship @!@! \n
        Enter p to play. Enter q to quit."
 
   selection = gets.chomp.upcase
 
-  if selection == "P"
-    ship_placement
-  elsif selection == "Q"
-    puts   'Farewell'
-  else
-    puts "That's an invalid selection, please try again."
-    selection = gets.chomp
-    main_menu
+    if selection == "P"
+      ship_placement
+    elsif selection == "Q"
+      puts   'Farewell'
+    else
+      puts "That's an invalid selection, please try again."
+      selection = gets.chomp
+      main_menu
+    end
   end
-end
 
-def ship_placement
-  puts   "Greetings hero! It appears you woke up on the wrong side of the deck today!\n
+  def ship_placement
+    puts   "Greetings hero! It appears you woke up on the wrong side of the deck today!\n
         Alas, we have no time to talk! The Dread Pirate Rob.... I'm sorry, Roald\n
         has ammassed their ships in the waters over yonder. We have but two in our fleet,\n
         and they are yours to command! First is our cruiser! Coming in at three coordinates long\n
@@ -72,27 +72,27 @@ def ship_placement
 
     puts @player_one.player_board.render
 
-  until @player_one.place_cruiser(gets.chomp.split)
+    until @player_one.place_cruiser(gets.chomp.split)
         loop
+    end
+
+    puts 'Your cruiser has been set'
+    puts @player_one.player_board.render(true)
+
+    puts "Now lets place our sub! Same rules as before,\n
+    but this time we only need two coordinates, and they can't overlap with the cruiser!"
+
+    until @player_one.place_sub(gets.chomp.split)
+      loop
+    end
+
+    puts 'Capital idea! I was thinking of placing it there myself!'
+    puts @player_one.player_board.render(true)
+
+    puts 'CAPTAIN! It seems Roald has moved their ships into position, get ready to attack!'
+    computer_places_ships
+    game_flow
   end
-
-  puts 'Your cruiser has been set'
-  puts @player_one.player_board.render(true)
-
-  puts "Now lets place our sub! Same rules as before,\n
-  but this time we only need two coordinates, and they can't overlap with the cruiser!"
-
-  until @player_one.place_sub(gets.chomp.split)
-    loop
-  end
-
-  puts 'Capital idea! I was thinking of placing it there myself!'
-  puts @player_one.player_board.render(true)
-
-  puts 'CAPTAIN! It seems Roald has moved their ships into position, get ready to attack!'
-  computer_places_ships
-  game_flow
-end
 
 def game_flow
   puts "It's time to really give them what fore! Select a single coordinate to fire on, and remeber, try not to repeat coordiantes!"
@@ -114,27 +114,23 @@ def game_flow
 end
 
 def game_over
-  if @computer.computer_cruiser.health == 0 && @computer.computer_sub.health == 0
-    puts 'We got Roald! Congratulations captain!'
-  elsif @player_one.player_cruiser.health == 0 && @player_one.player_sub.health == 0
-    puts 'They got us this time cap, but I know we can get them next time'
-end
-puts 'Play again? Y for yes and N for no.'
-  selection = gets.chomp.upcase
-  if selection == 'Y'
-    new_game
-    ship_placement
-  elsif selection == 'N'
-    puts 'Farewell'
-  else selection != 'Y' && selection != 'N'
-    puts "Again that's Y for yes and N for no"
-  end
-end
+    if @computer.computer_cruiser.health == 0 && @computer.computer_sub.health == 0
+      puts 'We got Roald! Congratulations captain!'
+    elsif @player_one.player_cruiser.health == 0 && @player_one.player_sub.health == 0
+      puts 'They got us this time cap, but I know we can get them next time'
+    end
 
-#this should include things such as a main menu
-# ship placement
-# firing
-# turns
-# define game over that takes in an argument or maybe make this an attribute and call it in a game
-#over method and then that will break my loop
+  puts 'Play again? Y for yes and N for no.'
+
+  selection = gets.chomp.upcase
+
+    if selection == 'Y'
+      new_game
+      ship_placement
+    elsif selection == 'N'
+      puts 'Farewell'
+    else selection != 'Y' && selection != 'N'
+      puts "Again that's Y for yes and N for no"
+    end
+  end
 end
