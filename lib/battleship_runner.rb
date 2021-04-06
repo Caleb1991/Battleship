67e9bda435fe643ea19444e88037538e6941c401
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-06 17:40:22 -0600

    Create instance of a game

--- lib/battleship_runner.rb
+++ lib/battleship_runner.rb
@@ -5,28 +5,6 @@ require './lib/game'
 require './lib/player'
 require './lib/ship'
 
-board = Board.new
+game = Game.new
 
-puts board.render
-
-crusier = Ship.new("Crusier", 3)
-
-board.place(crusier, ["C2", "C3", "C4"])
-
-puts board.render(true)
-
-puts board.render
-
-board.cells["A1"].fire_upon
-
-puts board.render
-
-board.cells["C2"].fire_upon
-board.cells["C3"].fire_upon
-
-puts board.render
-puts board.render(true)
-
-board.cells["C4"].fire_upon
-
-puts board.render
+game.main_menu
