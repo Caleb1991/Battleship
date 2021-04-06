cd5a7af1af8ea683acbe90dc324b67f339480216
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-06 17:43:28 -0600

    Create methods for game flow

--- lib/game.rb
+++ lib/game.rb
@@ -11,6 +11,38 @@ def initialize
   @computer = Computer.new
 end
 
+def computer_fire
+  choice = @computer.fire_coordinates.sample
+
+  @player_one.player_board.cells[choice].fire_upon
+  @computer.fire_coordinates.delete(choice)
+end
+
+def player_fire(coordinate)
+  if @computer.computer_board.valid_coordinate(coordinate) && @computer.computer_board.cells[coordinate].fired_upon? == false
+    @computer.computer_board.cells[coordinate].fire_upon
+    true
+  else
+    puts 'Sorry, that coordinate does not exist, or has already been fired upon, please enter another'
+    false
+  end
+end
+
+
+def new_game
+  @player_one = Player.new
+  @computer = Computer.new
+end
+
+def computer_places_ships
+  @computer.create_horizontal_cruiser_coordinates
+  @computer.create_vertical_cruiser_coordinates
+  @computer.create_horizontal_sub_coordinates
+  @computer.create_vertical_sub_coordinates
+  @computer.place_cruiser
+  @computer.place_sub
+end
+
 def main_menu
  puts "Welcome to....!@!@ Battleship @!@! \n
        Enter p to play. Enter q to quit."
@@ -29,7 +61,7 @@ def main_menu
 end
 
 def ship_placement
-  puts    "Greetings hero! It appears you woke up on the wrong side of the deck today!\n
+  puts   "Greetings hero! It appears you woke up on the wrong side of the deck today!\n
         Alas, we have no time to talk! The Dread Pirate Rob.... I'm sorry, Roald\n
         has ammassed their ships in the waters over yonder. We have but two in our fleet,\n
         and they are yours to command! First is our cruiser! Coming in at three coordinates long\n
@@ -56,15 +88,27 @@ def ship_placement
 
   puts 'Capital idea! I was thinking of placing it there myself!'
   puts @player_one.player_board.render(true)
+
+  puts 'CAPTAIN! It seems Roald has moved their ships into position, get ready to attack!'
+  computer_places_ships
   game_flow
 end
 
 def game_flow
-  puts "Now it's time to really give them what fore! Select a single coordinate to fire on, and remeber, try not to repeat coordiantes!"
+  puts "It's time to really give them what fore! Select a single coordinate to fire on, and remeber, try not to repeat coordiantes!"
 
   until @computer.computer_cruiser.health == 0 && @computer.computer_sub.health == 0 || @player_one.player_cruiser.health == 0 && @player_one.player_sub.health == 0
-
-  end
+    puts "******* Roald's Board *******"
+    puts @computer.computer_board.render
+    puts "\n ******* Your Board *******"
+    puts @player_one.player_board.render
+    puts 'Now where shall attack next?!'
+      until player_fire(gets.chomp)
+        loop
+      end
+      puts 'The enemy fired back!'
+      computer_fire
+      end
 
   game_over
 end
@@ -78,6 +122,7 @@ end
 puts 'Play again? Y for yes and N for no.'
   selection = gets.chomp.upcase
   if selection == 'Y'
+    new_game
     ship_placement
   elsif selection == 'N'
     puts 'Farewell'
