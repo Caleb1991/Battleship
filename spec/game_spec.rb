8cbe4d1a96284e28714353f641fdd0480b2d54e8
Caleb Wittman <calebjwittman1991@gmail.com>
2021-04-06 19:04:45 -0600

    Add missing tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -12,4 +12,32 @@ RSpec.describe Game do
 
     expect(game).to be_an_instance_of(Game)
   end
+
+  it 'has a computer' do
+    game = Game.new
+
+    expect(game.computer).to be_an_instance_of(Computer)
+  end
+
+  it 'has a player' do
+    game = Game.new
+
+    expect(game.player_one).to be_an_instance_of(Player)
+  end
+
+  it 'has a computer that fires' do
+    game = Game.new
+
+    expected = game.computer.fire_coordinates.length
+
+    game.computer_fire
+
+    expect(game.computer.fire_coordinates.length).to eq(expected - 1)
+  end
+
+  it 'has a player that can fire' do
+    game = Game.new
+
+    expect(game.player_fire('A1')).to eq(true)
+  end
 end
